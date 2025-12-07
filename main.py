7ee4fe6552d98da993372e234af2d2b36d7bfac7
zishan044 <winchesterfelix007@gmail.com>
2025-12-07 22:28:13 +0600

    implement generate answer node

--- main.py
+++ main.py
@@ -117,3 +117,21 @@ def rewrite_question(state: MessagesState):
     prompt = REWRITE_PROMPT.format(question=question)
     response = response_model.invoke([{"role": "user", "content": prompt}])
     return {"messages": [HumanMessage(content=response.content)]}
+
+GENERATE_PROMPT = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer the question. "
+    "If you don't know the answer, just say that you don't know. "
+    "Use three sentences maximum and keep the answer concise.\n"
+    "Question: {question} \n"
+    "Context: {context}"
+)
+
+
+def generate_answer(state: MessagesState):
+    """Generate an answer."""
+    question = state["messages"][0].content
+    context = state["messages"][-1].content
+    prompt = GENERATE_PROMPT.format(question=question, context=context)
+    response = response_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": [response]}
