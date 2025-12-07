0821b2f8fddf024d6794ea4e566adf9f5de1eb1b
zishan044 <winchesterfelix007@gmail.com>
2025-12-07 22:12:42 +0600

    implement query generation

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import getpass
-import os
 
 from langchain_classic.tools.retriever import create_retriever_tool
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_ollama import OllamaEmbeddings
+from langchain.chat_models import init_chat_model
+from langgraph.graph import MessagesState
 
 urls = [
     "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
@@ -37,4 +37,21 @@ retriever_tool = create_retriever_tool(
     "Search and return information about Lilian Weng blog posts.",
 )
 
-retriever_tool.invoke({"query": "types of reward hacking"})
+response_model = init_chat_model(model='llama3.1', temperature=0.2)
+
+def generate_query_or_respond(state: MessagesState):
+    response = response_model.bind_tools(tools=[retriever_tool]).invoke(state["messages"])
+    return { "messages": response }
+
+input = {"messages": [{"role": "user", "content": "hello!"}]}
+generate_query_or_respond(input)["messages"][-1].pretty_print()
+
+input = {
+    "messages": [
+        {
+            "role": "user",
+            "content": "What does Lilian Weng say about types of reward hacking?",
+        }
+    ]
+}
+generate_query_or_respond(input)["messages"][-1].pretty_print()
