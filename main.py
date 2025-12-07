8a999235198eaeb284824bb80dc723e5a3cba578
zishan044 <winchesterfelix007@gmail.com>
2025-12-07 21:45:11 +0600

    implement doccument preprocessing

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from custom-langgraph-rag-agent!")
+import getpass
+import os
 
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-if __name__ == "__main__":
-    main()
+# def _set_env(key):
+#     if key not in os.environ:
+#         os.environ[key] = getpass.getpass(f"{key}:")
+
+# _set_env('OPENAI_API_KEY')
+
+urls = [
+    "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
+    "https://lilianweng.github.io/posts/2024-07-07-hallucination/",
+    "https://lilianweng.github.io/posts/2024-04-12-diffusion-video/",
+]
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+doc_splits[0].page_content.strip()
